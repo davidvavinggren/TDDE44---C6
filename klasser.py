bfce84292dd92f9a17836d183e1cf4b147f2c3bf
jonassvedberg1 <55879806+jonassvedberg1@users.noreply.github.com>
2020-04-01 21:33:48 +0200

    hej

---
+++ klasser.py
@@ -0,0 +1,44 @@
+import math
+
+class Pet (object):
+    def __init__ (self, name = ""):
+        self.name = name
+        self.toy = []
+        self.kind = ""
+
+    def __str__(self):
+        if self.toy == []:
+            return "{} är en {} som inte har några leksaker.".format(self.name, self.kind)
+        return "{} är en {} som har följande leksaker: {}".format(self.name, self.kind, ", ".join(self.toy))
+
+    def add_toy(self, toy):
+        if toy not in self.toy:
+            self.toy.append(toy)
+
+
+class Vector2D (object):
+    def __init__ (self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return "(" + str(self.x) + "," + str(self.y) + ")"
+
+    def get_length (self):
+        return math.sqrt(((self.x)**2) + ((self.y)**2))
+
+    def add (self, v1):
+        self.x += v1.x
+        self.y += v1.y
+        return (self.x, self.y)
+
+    def add_to_new (self, v1):
+        return Vector2D (self.x + v1.x, self.y + v1.y)
+
+    def is_longer_than (self, v1):
+        return v1.get_length() < self.get_length()
+
+    def create_unit_vector (self):
+        norm_x = self.x / self.get_length()
+        norm_y = self.y / self.get_length()
+        return Vector2D(norm_x, norm_y)
