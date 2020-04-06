2610d232644afa1873584d01175f0305fb986982
David Vävinggren <61325141+davidvavinggren@users.noreply.github.com>
2020-04-06 11:24:34 +0200

    ok

--- Laborationer/Lab5/uppgift1.py
+++ Laborationer/Lab5/uppgift1.py
@@ -1,22 +1,22 @@
 from klasser import Pet
 
 
-p1 = Pet ("Snutte")
-p1.add_toy ("Boll")
-p1.add_toy ("Hagelbössa")
+p1 = Pet("Snutte")
+p1.add_toy("Boll")
+p1.add_toy("Hagelbössa")
 p1.kind = "katt"
 
-p2 = Pet ("Göran")
-p2.add_toy ("Blyertspenna")
-p2.add_toy ("Blyertspenna")
+p2 = Pet("Göran")
+p2.add_toy("Blyertspenna")
+p2.add_toy("Blyertspenna")
 p2.kind = "föräldralös"
 
-p3 = Pet ("Jonas")
+p3 = Pet("Jonas")
 p3.add_toy("Gaffel")
 p3.kind = ("pudel")
 
 list = [p1, p2]
 
-print (p1)
-print (p2)
-print (p3)
+print(p1)
+print(p2)
+print(p3)
