0f0c1b6b9bf361054026912729a841d1329a0556
jonassvedberg1 <55879806+jonassvedberg1@users.noreply.github.com>
2020-04-13 10:00:22 +0200

    peppat

--- Laborationer/Lab5/uppgift2.py
+++ Laborationer/Lab5/uppgift2.py
@@ -1,17 +1,17 @@
 from klasser import Vector2D as v2D
 
-vektor1 = v2D(4,5)
-vektor2 = v2D (3,2)
+vektor1 = v2D(4, 5)
+vektor2 = v2D(3, 2)
 
 print(vektor1.add(vektor2))
 
-print (vektor2.get_length())
+print(vektor2.get_length())
 
 
-print (vektor1.add_to_new(vektor2), vektor1, vektor2)
+print(vektor1.add_to_new(vektor2), vektor1, vektor2)
 
 
-print (vektor2.is_longer_than(vektor1))
+print(vektor2.is_longer_than(vektor1))
 
 
-print (vektor2.create_unit_vector())
+print(vektor2.create_unit_vector())
