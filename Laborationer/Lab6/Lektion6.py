cf4bb8e8c4f1a0f1cd350b81502fa8cd903af995
jonassvedberg1 <55879806+jonassvedberg1@users.noreply.github.com>
2020-04-17 14:34:02 +0200

    Labb6

--- Laborationer/Lab6/Lektion6.py
+++ Laborationer/Lab6/Lektion6.py
@@ -3,3 +3,27 @@ class Contact(object):
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
+
+    def __str__(self):
+        string = "{}, {}"
+        return string.format(self.last_name, self.first_name)
+
+class ContactList(object):
+
+    def __init__(self):
+        self.contacts = []
+
+    def create_and_add_contact(self, first_name, last_name):
+        self.contacts.append(Contact(first_name, last_name))
+
+    def __str__(self):
+        str = ""
+        for contact in self.contacts:
+            str += contact.__str__() + "\n"
+        return str[:-1]
+
+contactlist = ContactList()
+contactlist.create_and_add_contact("Ada", "Lovelace")
+contactlist.create_and_add_contact("Alan", "Turing")
+contactlist.create_and_add_contact("Grace", "Hopper")
+print(contactlist)
---
+++ Laborationer/Lab6/del1.py
@@ -0,0 +1,5 @@
+file = open("Lorem.txt", "r")
+text_fil = file.read()
+file.close()
+
+print(text_fil) 
