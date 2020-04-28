72d2b0dbf56862d76ef5ed3195f6e4b2367e86a1
jonassvedberg1 <55879806+jonassvedberg1@users.noreply.github.com>
2020-04-28 11:18:27 +0200

    del1,2 pep

--- Laborationer/Lab6/del1.py
+++ Laborationer/Lab6/del1.py
@@ -1,79 +1,108 @@
+"""TDDE44, Labb 6, Del 1."""
+
 
 class Text(object):
+    """Klassen Text."""
 
     def __init__(self, value):
+        """Initiera instansen och ta in texten i fråga."""
         self.text = value
         self.text_split = value.split("\n")
 
     def create_list_of_sentences(self):
+        """Dela upp texten i meningar och placera meningarna i en lista."""
         list_of_sentences = []
+        # addera meningarna till en lista
         for sentence in self.text_split:
             list_of_sentences.append(Sentence(sentence))
         return list_of_sentences
 
     def count_amount_of_sentences(self):
+        """Beräkna antalet meningar."""
         return len(self.text_split)
 
     def count_amount_of_words_tot(self):
+        """Beräkna antalet ord i texten."""
         sum_of_words = 0
+        # För varje mening; addera summan av ord till en totalsumma
         for sentence in self.create_list_of_sentences():
             sum_of_words += sentence.count_amount_of_words()
         return sum_of_words
 
     def count_amount_of_tokens_tot(self):
+        """Beräkna antalet tecken i texten."""
         sum_of_tokens = 0
+        # För varje mening; addera summan tecken till en totalsumma
         for sentence in self.create_list_of_sentences():
             sum_of_tokens += sentence.count_amount_of_tokens()
         return sum_of_tokens
 
     def __str__(self):
+        """Printa enligt anvisning."""
         index = 1
-        str1 = "Texten innehåller {} meningar, {} ord/skiljetecken, {} tecken. \n"
+        str1 = "Texten innehåller {} meningar, {} ord/skiljetecken, \
+               {} tecken.\n"
         str2 = "Mening {} "
         string_to_return = ""
+        # loopa genom meningarna och använd deras räknemetoder
         for sentence in self.create_list_of_sentences():
-            string_to_return += str2.format(str(index)) + sentence.__str__() + "\n"
-            index +=1
+            str_to_return += (str2.format(str(index)) + sentence.__str__() +
+                              "\n")
+            index += 1
         return str1.format(self.count_amount_of_sentences(),
                            self.count_amount_of_words_tot(),
-                           self.count_amount_of_tokens_tot()) + string_to_return
+                           self.count_amount_of_tokens_tot()) + str_to_return
 
 
 class Sentence(object):
+    """Klassen Sentence."""
 
     def __init__(self, value):
+        """Initiera sentence, ta med mening och skapa en lista med ord."""
         self.sentence = value
         self.split_sentence = self.sentence.split(" ")
 
     def create_list_of_words(self):
+        """Gör en lista med tokenobjekt och returna den."""
         list_of_words = []
+        # loopa genom listan med ord
         for word in self.split_sentence:
             list_of_words.append(Token(word))
         return list_of_words
 
     def count_amount_of_words(self):
+        """Räkna ihop alla ord i meningen."""
         return len(self.split_sentence)
 
     def count_amount_of_tokens(self):
+        """Räkna ihop summan av alla tecken i ordlistan."""
         sum_of_tokens = 0
+        # loopa genom alla ord i listan
         for word in self.create_list_of_words():
             sum_of_tokens += word.count_amount_of_tokens()
         return sum_of_tokens
 
     def __str__(self):
+        """Printa enligt nedan, använd räknemetoderna."""
         str = "innehåller {} ord/skiljetecken ({} tecken)"
         return str.format(self.count_amount_of_words(),
                           self.count_amount_of_tokens())
 
+
 class Token(object):
+    """Klassen token."""
 
     def __init__(self, value):
+        """Initiera och ta in ett ord."""
         self.word = value
 
     def count_amount_of_tokens(self):
+        """Räkna hur många tecken ett ord har."""
         return len(self.word)
 
+
 def Main():
+    """Huvudfunktion där allt körs."""
     file = open("Lorem.txt", "r")
     text_file = file.read()
     file.close()
@@ -81,4 +110,5 @@ def Main():
     if __name__ == "__main__":
         print(text)
 
-Main ()
+Main()
+"""Kalla på Huvudfunktion där allt körs."""
--- Laborationer/Lab6/del2.py
+++ Laborationer/Lab6/del2.py
@@ -1,80 +1,106 @@
+"""TDDE44, Labb 6, Del 2."""
+
+
 class TodoApp (object):
+    """Klassen TodoApp."""
 
     def __init__(self):
-        self.commands = {"ny" : self.new_task, "visa" : self.show_tasks,
-                         "klar" : self.mark_done, "?" : self.show_commands}
+        """Initiera instansen med tillhörande instansvariabler."""
+        self.commands = {"ny": self.new_task, "visa": self.show_tasks,
+                         "klar": self.mark_done, "?": self.show_commands}
         self.task_list = TaskList()
         self.run()
-        #kommando = kommando
 
-    def run (self):
+    def run(self):
+        """Kör enligt nedanstående anvisning."""
+        # kör loopen alltid såvida inte if-satsen uppfylls
         while True:
             user_input = input("Ange kommando (q=avsluta ?=hjälp) ")
+            # stäng ner om man skriver q
             if user_input == "q":
                 break
+            # pröva om ett kommando finns i kommandolistan
             try:
                 self.commands[user_input]()
-
+            # skriv ut om try misslyckas
             except KeyError:
                 print("Inget kommando")
 
     def show_commands(self):
+        """Printa de olika kommandon som finns."""
         print("Kommandon: ny, visa, klar, ?")
 
     def new_task(self):
+        """Gör ett nytt taskobjekt."""
         new_task = input("Beskriv uppgiften: ")
         self.task_list.create_task(new_task)
 
     def show_tasks(self):
-        print(self.task_list.__str__())
+        """Visa de tasks som lagts till."""
+        print(self.task_list)
 
     def mark_done(self):
-        print(self.task_list.__str__())
+        """Skriv ut de som finns och kör mark_done på den utpekade."""
+        print(self.task_list)
         finished_task_id = input("Vilken uppgift ska markeras som klar? ")
         self.task_list.mark_done(int(finished_task_id))
 
 
 class TaskList (object):
+    """Klassen TaskList."""
 
     def __init__(self):
-        #self.task_counter = len(task_list)
+        """Initiera instansten med tillhörande instansvariabler."""
         self.task_list = {}
-        #self.task_id = len(self.task_list)
+        self.task_counter = 0
 
     def create_task(self, new_task):
-        #print("Test")
-        self.task_list[new_task] = Task(new_task, len(self.task_list))
-
+        """Lägg till i dictet och skapa ett task samt räkna upp antalet."""
+        self.task_list[new_task] = Task(new_task, self.task_counter)
+        self.task_counter += 1
 
     def mark_done(self, finished_task_id):
+        """Gå igenom uppgifterna och kör mark_done på rätt."""
+        # loopa genom uppgifterna för att hitta rätt
         for task in self.task_list.values():
+            # hitta rätt uppgift och kör mark_done på den
             if task.task_id == finished_task_id:
                 task.mark_done()
 
     def __str__(self):
+        """Printa enligt taskobjektens __str__."""
+        # skriv ut om listan är tom
         if self.task_list == {}:
             print("Listan är tom")
             return ""
         string = ""
+        # loopa genom uppgiftobjekten och skriv ut deras prints
         for task in self.task_list.values():
             string += task.__str__()
         return string[:-1]
 
+
 class Task(object):
+    """Klassen Task."""
 
     def __init__(self, task_description, task_id):
+        """Initiera taskklassen och skicka in namnet samt siffran."""
         self.task_id = task_id
         self.task_description = task_description
         self.done = False
 
     def mark_done(self):
+        """Ändra instansvariabeln done till True."""
         self.done = True
 
     def __str__(self):
+        """Printa enligt anvisning."""
         string = "{}. [{}] {} \n"
         if self.done:
+            # skriv ut med kryss om uppgiften är klar annars utan
             return string.format(self.task_id, "X", self.task_description)
         return string.format(self.task_id, " ", self.task_description)
 
 if __name__ == "__main__":
+    """Huvudfunktion där man kör."""
     todo_app = TodoApp()
