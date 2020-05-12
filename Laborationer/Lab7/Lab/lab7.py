c6afc3f8e89229bdbbf7b4db00a0ea16fa7b05a2
jonassvedberg1 <55879806+jonassvedberg1@users.noreply.github.com>
2020-05-12 08:15:42 +0200

    grejs

--- Laborationer/Lab7/Lab/lab7.py
+++ Laborationer/Lab7/Lab/lab7.py
@@ -1,20 +1,42 @@
-import time
+from time import time
+import re
 import argparse
 from med import minimum_edit_distance
 import sys
 
 class SpellingWarning(object):
 
-    def __init__(self, text):
-        self.text = text
+    def __init__(self, text, lexicon):
+        self.lexicon = lexicon
+        self.file = open(text, "r", encoding = "utf-8")
+        self.text = self.file.read()
+        self.file.close()
+        self.epic_text = self.text_modifier()
 
     def text_modifier(self):
-        text_to_modify = open(self.text, encoding = "utf-8")
-        list_to_modify = str(text_to_modify).rstrip("\n").split(" ")
+        string_to_modify = str(self.text).replace("\n", " ").lower()
+        list_to_modify = re.sub("[\W_]\-", " ", string_to_modify).split(" ")
         return list_to_modify
 
+    def comparer(self):
+        n = 0
+        for word in self.epic_text:
+            #n += 1
+            #print(word)
+            for lex_word in self.lexicon:
+                if word in lex_word[0]:
+                    n += 1
+                    #print(word)
+                    break
+                elif word not in lex_word[0]:
+                    #n += 1
+                    print(word) #str(n))
+                    edit_distance = minimum_edit_distance(word, lex_word)
+                    break
+
     def __str__(self):
-        return str(self.text_modifier())
+        pass
+        #return str(self.text_modifier())
 
 
 class Report(object):
@@ -44,11 +66,15 @@ class Lexicon(object):
 
 
 def main():
+    start_time = time()
     lexicon_name = sys.argv[1]
     lexicon = Lexicon(lexicon_name)
-    lexicon.load_freq_data(lexicon_name)
+    print(lexicon.load_freq_data(lexicon_name))
     #print(word_freq)
-    spelling_warning = SpellingWarning("kort1.txt")
-    print(spelling_warning)
+    #spelling_warning = SpellingWarning("kort1.txt", lexicon.load_freq_data(lexicon_name))
+    #spelling_warning.comparer()
+    run_time = time() - start_time
+    print(str(run_time))
+
 
 main()
